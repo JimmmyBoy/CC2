31914350b539ffee339edb647f5d618881471d79
Erwan mou <terwan28@gmail.com>
2023-01-18 11:40:28 +0100

    livraison

--- src/Modele/Livraison.java
+++ src/Modele/Livraison.java
@@ -3,12 +3,7 @@ package Modele;
 import java.sql.Date;
 import java.sql.Time;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "livraison")
@@ -17,10 +12,30 @@ public class Livraison {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_livraison")
 	private int Id_livraison;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "date_livraison")
 	private Date Date_livraison;
+	
+	@Temporal(TemporalType.TIME)
+	@Column(name = "heure_livraison")
 	private Time Heure_livraison;
+	
+	@OneToOne(mappedBy = "livraison")
 	private int Id_contrat;
 
+	public Livraison(int id_livraison, Date date_livraison, Time heure_livraison, int id_contrat) {
+		super();
+		Id_livraison = id_livraison;
+		Date_livraison = date_livraison;
+		Heure_livraison = heure_livraison;
+		Id_contrat = id_contrat;
+	}
+
+	public Livraison() {
+		
+	}
+	
 	public int getId_livraison() {
 		return Id_livraison;
 	}
